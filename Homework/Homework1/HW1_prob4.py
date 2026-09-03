ccb22ab5582e2c9f5885d1f9318e20d70e09ff3f
cooper599 <cowa7997@colorado.edu>
2026-09-03 16:51:52 -0600

    Rel errors for part b prob 4

--- Homework/Homework1/HW1_prob4.py
+++ Homework/Homework1/HW1_prob4.py
@@ -22,3 +22,23 @@ rel_err_r2 = abs(r2_approx-r2_true)/abs(r2_approx)
 print("True r1:", r1_true, ", True r2:", r2_true)
 print("Approx r1:", r1_approx, ", Approx r2:", r2_approx)
 print("Relative Error r1:", rel_err_r1, ", Relative Error r2:", rel_err_r2)
+
+# Part b
+# Found relations: 
+# r1+r2 = -b/a
+#r1r2=c/a
+
+sum_r2_true = -b/a - r1_true
+sum_r2_approx = -b/a - r1_approx
+
+prod_r2_true = 1/r1_true
+prod_r2_approx = 1/r1_approx
+
+rel_err_true_sum = abs(sum_r2_true-r2_true)/abs(r2_true)
+rel_err_approx_sum = abs(sum_r2_approx-r2_true)/abs(r2_true)
+
+rel_err_true_prod = abs(prod_r2_true-r2_true)/abs(r2_true)
+rel_err_approx_prod = abs(prod_r2_approx-r2_true)/abs(r2_true)
+
+print("Sum relation relative errors: True", rel_err_true_sum, ", Approx:", rel_err_approx_sum)
+print("Prod relation relative errors: True", rel_err_true_prod, ", Approx:", rel_err_approx_prod)
