cbee4a69f4df3c74366aaf86e9e27c060dd1d88c
cooper599 <cowa7997@colorado.edu>
2026-09-03 18:27:02 -0600

    updated prob 5 code through part c

--- Homework/Homework1/HW1_prob5.py
+++ Homework/Homework1/HW1_prob5.py
@@ -11,10 +11,33 @@ delta_arr = np.logspace(-16,0,17)
 
 x_arr = [np.pi, 10**6]
 
-plt.semilogx(delta_arr,cosEquation(x_arr[0],delta_arr)-cosExpression(x_arr[0],delta_arr),label="x=pi")
-plt.semilogx(delta_arr,cosEquation(x_arr[1],delta_arr)-cosExpression(x_arr[1],delta_arr),label="x=10^6")
+diff_pi = cosEquation(x_arr[0],delta_arr)-cosExpression(x_arr[0],delta_arr)
+diff_106 = cosEquation(x_arr[1],delta_arr)-cosExpression(x_arr[1],delta_arr)
+
+print("Difference when x = pi:", diff_pi)
+print("Difference when x = 10^6:", diff_106)
+                                                        
+plt.semilogx(delta_arr,diff_pi,label="x=pi")
+plt.semilogx(delta_arr,diff_106,label="x=10^6")
 plt.xlabel("deltas")
 plt.ylabel("Difference between equation and manipulated expression")
 plt.title("Difference between equation and manupulated expression for range of deltas")
 plt.legend()
 plt.show()
+
+# Part c, using same x_arr and delta_arr
+cos_Taylor = lambda x, delta: -delta*np.sin(x)-(delta**2)/2 * np.cos(x)
+
+diff_myalg_pi = cos_Taylor(x_arr[0],delta_arr)-cosExpression(x_arr[0],delta_arr)
+diff_myalg_106 = cos_Taylor(x_arr[1],delta_arr)-cosExpression(x_arr[1],delta_arr)
+
+plt.semilogx(delta_arr,diff_myalg_pi,label="x=pi")
+plt.semilogx(delta_arr,diff_myalg_106,label="x=10^6")
+plt.xlabel("deltas")
+plt.ylabel("Difference between equation and manipulated expression")
+plt.title("Difference between Taylor Algorithm and manipulated expression for range of deltas")
+plt.legend()
+plt.show()
+
+print("Taylor exp Difference when x = pi:", diff_myalg_pi)
+print("Taylor exp Difference when x = 10^6:", diff_myalg_106)
